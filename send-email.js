4780527f540e2051f9473f336161133d0455c569
Carlos Axel Cruz Bon <axel.bon.cc@gmail.com>
2020-07-08 04:07:25 -0500

    a

--- send-email.js
+++ send-email.js
@@ -1,6 +1,6 @@
 var AWS = require('aws-sdk');
 // Set the region 
-AWS.config.update({ 
+AWS.config.update({
     region: 'us-east-2',
     credentials: new AWS.SharedIniFileCredentials()
 });
@@ -8,8 +8,11 @@ AWS.config.update({
 // Create sendEmail params 
 var params = {
     TemplateName: 'testtemplate' /* required */
-  };
-  ses.getTemplate(params, function(err, data) {
+};
+
+var ses = new AWS.SES();
+
+ses.getTemplate(params, function (err, data) {
     if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-  });
+    else console.log(data);           // successful response
+});
