49fe68b0fefc468f1d483d23ecdfe013caf5e87d
Carlos Axel Cruz Bon <axel.bon.cc@gmail.com>
2020-07-08 12:04:21 -0500

    a

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/send-email', (req, res) => {
 });
 
 app.get('/stats', (req, res)=>{
-  console.log('test pls pls pls pls');
+  console.log(__dirname + '\\html\\images');
     res.status(200).send('ESTO ES UN TEST !@#');
 });
 
