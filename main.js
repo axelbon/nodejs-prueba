7dd8c1bd44f83079aed9f2b1edb75a8941095e6c
Carlos Axel Cruz Bon <axel.bon.cc@gmail.com>
2020-07-07 23:23:02 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+var nodemailer = require('nodemailer');
+var express = require('express');
+var app = express();
+app.use(express.json());
+
+app.post('/send-email', (req, res) => {
+    console.log(req.body);
+
+    var transport = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'connor49@ethereal.email',
+            pass: 'MC5secr3uZEFGbVhVh',
+
+        }
+    });
+
+    var mailOptions = {
+        from: 'Remitente',
+        to: 'axel.bon.cc@gmail.com',
+        subject: 'Enviado desde nodemailer',
+        text: 'HELLO WORLD @!#@!LJHK#LK!@#J@L!#K#KJL!@#POIDS@!*(#)@!*()#@'
+    };
+
+    transport.sendMail(mailOptions, (error, info) => {
+        if(error){
+            res.status(500).send(error.message);
+        }else{
+            console.log('Email enviado.');
+            res.status(200).json(req.body);
+        }
+
+    });
+
+});
+
+app.get('/stats', (req, res)=>{
+    res.status(200).send('ESTO ES UN TEST !@#');
+});
+
+app.listen(5000, () => {
+    console.log('servidor en -> http://localhost:5000');
+});
+
+
