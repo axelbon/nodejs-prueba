d357413ca1c5c006e6885dee3cfced7db3a52cce
Carlos Axel Cruz Bon <axel.bon.cc@gmail.com>
2020-07-08 17:16:34 -0500

    a

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var nodemailer = require('nodemailer');
 var express = require('express');
 
+const fs = require('fs');
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -12,47 +14,54 @@ app.use(cors());
 
 
 app.post('/send-email', (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
 
-    var transport = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false,
-        auth: {
-            user: 'connor49@ethereal.email',
-            pass: 'MC5secr3uZEFGbVhVh',
+  var transport = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'connor49@ethereal.email',
+      pass: 'MC5secr3uZEFGbVhVh',
 
-        }
-    });
+    }
+  });
 
-    var mailOptions = {
-        from: 'Remitente',
-        to: 'axel.bon.cc@gmail.com',
-        subject: 'Enviado desde nodemailer',
-        text: 'HELLO WORLD @!#@!LJHK#LK!@#J@L!#K#KJL!@#POIDS@!*(#)@!*()#@'
-    };
+  var mailOptions = {
+    from: 'Remitente',
+    to: 'axel.bon.cc@gmail.com',
+    subject: 'Enviado desde nodemailer',
+    text: 'HELLO WORLD @!#@!LJHK#LK!@#J@L!#K#KJL!@#POIDS@!*(#)@!*()#@'
+  };
 
-    transport.sendMail(mailOptions, (error, info) => {
-        if(error){
-            res.status(500).send(error.message);
-        }else{
-            console.log('Email enviado.');
-            res.status(200).json(req.body);
-        }
+  transport.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(500).send(error.message);
+    } else {
+      console.log('Email enviado.');
+      res.status(200).json(req.body);
+    }
 
-    });
+  });
 
 });
 
-app.get('/stats', (req, res)=>{
-  console.log(__dirname + '/html/images');
-    res.status(200).send('ESTO ES UN TEST !@#');
+app.get('/stats', (req, res) => {
+  var url_image = __dirname + '\\html\\images\\avatar.png';
+
+  if(fs.existsSync(url_image)){
+    console.log('existe');
+  }else {
+    console.log('no existe');
+  } 
+
+  res.status(200).json(url_image);
 });
 
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3100;
 console.log(port);
 const server = app.listen(port, () => {
-    console.log('Server listening on port ' + port);
+  console.log('Server listening on port ' + port);
 });
 
 
