875cf6edc10db7dc44568b28d59781ca64518c76
Carlos Axel Cruz Bon <axel.bon.cc@gmail.com>
2020-07-08 03:20:36 -0500

    a

--- send-email.js
+++ send-email.js
@@ -37,6 +37,6 @@ var params = {
 var ses = new AWS.SES();
 
 ses.sendEmail(params, function (err, data) {
-    if (err) console.log('error'); // an error occurred
+    if (err) console.log(err, err.stack); // an error occurred
     else console.log(data);           // successful response
 });
