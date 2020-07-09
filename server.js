f7f90a705be9978125702b77977fe4a72f291e90
Carlos Axel Cruz Bon <axel.bon.cc@gmail.com>
2020-07-08 20:00:19 -0500

    codigo para realizar el envio del mail

--- server.js
+++ server.js
@@ -466,6 +466,13 @@ app.post('/send-email', (req, res) => {
     Source: "email_sender_nodejs_teamknow@yahoo.com",
   };
 
+  var ses = new AWS.SES();
+
+  ses.sendEmail(params, function (err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else console.log(data);           // successful response
+  });
+
 
 });
 
